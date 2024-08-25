411a53dc67ce7a8dcbfd6ef571d77490ccbbab0e
BrownKnight <amanrk@hotmail.co.uk>
2024-08-25 17:59:39 +0100

    Adding proper links to homepage

--- ui/src/Home.tsx
+++ ui/src/Home.tsx
@@ -1,13 +1,19 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  const NavButton = ({ to, children }: { to: string } & React.PropsWithChildren) => {
+    return (
+      <Link to={to} className="h-16 p-4 rounded-lg bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white text-center content-center">
+        {children}
+      </Link>
+    );
+  };
   return (
-    <div className="flex flex-col max-w-screen-md mx-auto">
-      <h1 className="text-xl font-bold text-center">Grid Battle</h1>
-
-      <Link to="/battle">Create a Battle</Link>
-      <Link to="/battle">Join a Battle</Link>
-      <Link to="/grid/random">Random Grid</Link>
+    <div className="flex flex-col max-w-screen-md mx-auto gap-4 mt-4">
+      <NavButton to="/battle">Create a Battle</NavButton>
+      <NavButton to="/battle">Join a Battle</NavButton>
+      <NavButton to="/grid/random">Random Grid</NavButton>
     </div>
-  )
+  );
 }
