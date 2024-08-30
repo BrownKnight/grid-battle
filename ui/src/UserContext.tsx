d994c27e618a36b38e97a7e0c52dfd19bc86ced5
BrownKnight <amanrk@hotmail.co.uk>
2024-08-30 17:43:15 +0100

    Test password behaviour with new  navigate

--- ui/src/UserContext.tsx
+++ ui/src/UserContext.tsx
@@ -66,7 +66,7 @@ export default function UserContextProvider({ children }: React.PropsWithChildre
 
   const onClose = () => {
     setIsLoginOpen(false);
-    navigate("/");
+    navigate("#");
   };
 
   return (
