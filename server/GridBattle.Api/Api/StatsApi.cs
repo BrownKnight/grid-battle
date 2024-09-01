f96b571d7d6f1fd1eefa32844472b2d3e6ed8570
BrownKnight <amanrk@hotmail.co.uk>
2024-09-01 12:02:55 +0100

    Add more stats to the stats API

--- server/GridBattle.Api/Api/StatsApi.cs
+++ server/GridBattle.Api/Api/StatsApi.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GridBattle.Api;
 
-public sealed record StatsDto(string Version, int GridCount, int TimerBattleCount);
+public sealed record StatsDto(string Version, int GridCount, int TimerBattleCount, int UserCount);
 
 public static class StatsApi
 {
@@ -19,6 +19,7 @@ public static class StatsApi
         var version = ThisAssembly.AssemblyInformationalVersion;
         var gridCount = await dbContext.Grids.CountAsync();
         var timerBattleCount = await dbContext.TimerBattleRooms.CountAsync();
-        return TypedResults.Ok(new StatsDto(version, gridCount, timerBattleCount));
+        var userCount = await dbContext.Users.CountAsync();
+        return TypedResults.Ok(new StatsDto(version, gridCount, timerBattleCount, userCount));
     }
 }
